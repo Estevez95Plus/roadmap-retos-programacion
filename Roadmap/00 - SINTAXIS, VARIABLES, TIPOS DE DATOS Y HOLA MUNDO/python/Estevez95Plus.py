4f93fab1fb1f378a67a0f1dcc52f1a594628837f
Estevez95Plus <estevezperezandres@gmail.com>
2024-11-02 12:15:59 -0500

    Commit inicial del proyecto

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Estevez95Plus.py
@@ -0,0 +1,71 @@
+#Primer ejercicio. Tipos de comentarios
+
+"""Lenguaje seleccionado Python"""
+
+# https://www.python.org/
+
+#____________________________________________________________________________________________________
+
+"""SINTAXIS PARA LA CREACIÓN DE COMENTARIOS"""
+
+# Este es un comentario de una línea
+
+
+
+'''Comentario de varias líneas'''
+
+"""
+Comentario de varias líneas
+con comillas
+dobles
+
+"""
+
+'''
+Comentario de varias líneas
+ con comillas
+ simples
+ '''
+#____________________________________________________________________________________________________
+
+# Variables
+
+mi_variable = "Esto es una variable"  # Variable de tipo cadena
+
+#Variable tipo entero
+int= 10
+#Valor negativo
+int_neg= -10
+
+# Variable de tipo flotante
+float = 3.1416  
+#Valor negativo
+float_neg= -0.25
+
+# Variable de tipo booleano
+
+bool = True  # Valor verdadero
+bool_neg = False  # Valor falso
+
+# Variable de tipo string
+
+str = "Cadena de texto con comillas dobles"
+other_str = 'Cadena de texto con comillas simples'
+print(str)
+print(other_str)
+#____________________________________________________________________________________________________
+
+
+print ("¡Hola Python!")
+
+
+#____________________________________________________________________________________________________
+
+print (type(int))
+print (type(int_neg))
+print (type(float))
+print (type(float_neg))
+print (type(bool))
+print (type(bool_neg))
+print (type(str))
+print (type(other_str))
